979fa5a1b2f7d43d3158ee0806ebfb5bc98d39e3
dani <dnlvstn@gmail.ru>
2023-05-23 14:45:25 +0300

    было бы славно тесты написать для всего этого безобразия

--- Tasks/brackets and labyrinth.py
+++ Tasks/brackets and labyrinth.py
@@ -1,9 +1,12 @@
 from lsq import mycollections as mc
 import numpy as np
+import re
 import sys
 
 
 def brackets(input_text):
+    """Finds unused parentheses and prints out how many and where"""
+
     # every line -> list of chars
     input_chars = [list(each_line) for each_line in input_text]
 
@@ -48,38 +51,83 @@ def brackets(input_text):
             print(f"There are {len(closing)} unused closing bracket in the {closing}\n")
 
 
-    def path(labyrinth, start, finish, my_path):
-        me = start
-        for move in my_path:
-            match move:
-                case "up":
-                    pass
-                case "down":
-                    pass
-                case "left":
-                    pass
-                case "right":
-                    pass
+def path(labyrinth, start, finish, my_path):
+    """Tells you if you are going to find an exit from the labyrinth"""
+    def compare(pos, lim):
+        """Checks if you are going to the walls of the labyrinth"""
+        if pos not in range(0, lim) or labyrinth[y_pos][x_pos] == 1:
+            print('You are going the wrong way')
+            return 1
+        elif [y_pos, x_pos] == fi:
+            print("You found the exit")
+            return 0
+
+    # Reads your start and finish positions (as first two numbers anywhere in the string) and your route.
+    # It can be written in any way you want but must contain command words like: left, l, right, r, up, u, down, d
+    reg = r"\bup\b|\bu\b|\bdown\b|\bd\b|\bleft\b|\bl\b|\bright\b|\br\b"
+    st = [int(char) for char in re.findall(r'\d+', start, re.I)[:2:]]
+    fi = [int(char) for char in re.findall(r'\d+', finish, re.I)[:2:]]
+    route = [char.lower() for char in re.findall(reg, my_path, re.I)]
+
+    # Current position
+    y_pos, x_pos = st[0], st[1]
+
+    # Checks if start position is in the wall
+    if labyrinth[y_pos][x_pos] == 1:
+        raise Exception('Are you a victim of Philadelphia experiment?')
+
+    # Some boundaries of the labyrinth (right and bottom)
+    y_lim = np.shape(labyrinth)[0]
+    x_lim = np.shape(labyrinth)[1]
+
+    # Changes our position depending on the move we make
+    for move in route:
+        match move:
+            case 'up' | 'u':
+                y_pos -= 1
+                if compare(y_pos, y_lim) == 1:
+                    return
+
+            case 'down' | 'd':
+                y_pos += 1
+                if compare(y_pos, y_lim) == 1:
+                    return
+
+            case 'left' | 'l':
+                x_pos -= 1
+                if compare(x_pos, x_lim) == 1:
+                    return
+
+            case "right" | "r":
+                x_pos += 1
+                if compare(x_pos, x_lim) == 1:
+                    return
+
+    # If the whole algorithm is done, but we didn't reach the exit or the wall
+    if [y_pos, x_pos] != fi:
+        print('Where are you?')
 
 
 if __name__ == "__main__":
 
-    # Test for brackets()
+    # ___________________Test for brackets()____________________
     print("Text with parentheses: ")
 
     my_input = sys.stdin.readlines()
 
     brackets(my_input)
 
-    # Test for labyrinth
+    # ____________________Test for labyrinth_____________________
     laby = np.array([[1, 1, 1, 1, 0, 1],
                     [1, 0, 0, 0, 0, 1],
                     [1, 1, 0, 1, 0, 1],
-                    [1, 0, 0, 1, 1, 1]])
+                    [1, 0, 0, 1, 1, 1],
+                    [1, 1, 0, 1, 1, 1]])
 
-    start = laby[0][4]
-    finish = [3][1]
+    my_start = input("Your start point: ")
+    my_finish = input("Your exit from the labyrinth: ")
+    da_way = input("Your path: ")
 
-    my_path = input("Your path: ").split(' ')
+    path(laby, my_start, my_finish, da_way)
 
     print(laby[1][2])
